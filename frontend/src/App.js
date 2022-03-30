b64ca082ec127aef752c7bdd10335d5b203f8760
LucasbarthTI <lucasbarth@ienh.com.br>
2022-03-30 12:00:45 -0300

    Lucas Barth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,47 @@
-import React from 'react'
-import ListaProdutos from './ListaProdutos'
+import { Component } from "react";
+import api from "./api";
+import app from './App.css'
 
-export default function App(){
-  return (
-    <>
-    <ListaProdutos/>
-    </>
-  )
+
+class App extends Component{
+
+  state= {
+    produtos: [],
+  }
+
+  async componentDidMount(){
+    const response = await api.get('');
+
+    this.setState({produtos: response.data})
+   
+  }
+
+  render(){
+
+    const { produtos } = this.state;
+    return(
+      <div>
+        <h1>Listar os produtos</h1>
+        {console.log(produtos)}
+        {produtos.map(produto => (
+          <li className="Produtos" key={produto.id}>
+            <h2 className="Title">
+              <strong>Codigo: </strong>
+              {produto.codigo} 
+            </h2>
+            <p>
+            <strong>Descrição: </strong>
+              {produto.descricao}
+              <button className="btn" href="#">Editar</button>
+              <button className="btn">excluir</button>
+              <button className="btn">detalhes</button>
+            </p>
+
+          </li>
+        ))}
+      </div>
+    )
+  }
 }
+
+export default App;
